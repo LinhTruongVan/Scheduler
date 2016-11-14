c5256d83278624c3c415004b8d0700ad55ae8fc9
Linh Truong <linh.truong@orientsoftware.net>
2016-11-14 08:14:23 +0700

    Config CORS for web api.

--- Scheduler/App_Start/WebApiConfig.cs
+++ Scheduler/App_Start/WebApiConfig.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Http;
+﻿using Microsoft.Owin.Security.OAuth;
 using System.Web.Http;
-using Microsoft.Owin.Security.OAuth;
-using Newtonsoft.Json.Serialization;
+using System.Web.Http.Cors;
 
 namespace Scheduler
 {
@@ -12,6 +8,9 @@ namespace Scheduler
     {
         public static void Register(HttpConfiguration config)
         {
+            // Enable cross-domain
+            config.EnableCors(new EnableCorsAttribute("*", "*", "*"));
+
             // Web API configuration and services
             // Configure Web API to use only bearer token authentication.
             config.SuppressDefaultHostAuthentication();
